891f13d6d9427a9023326030c9cbcf6eb6aa6396
mahimagontalwar <70761344+mahimagontalwar@users.noreply.github.com>
2024-09-28 21:22:57 +0530

    Update MapComponent.js

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
-const MapComponent = ({ location }) => {
-  const mapStyles = {        
-    height: "400px",
-    width: "100%"};
-  
-  const defaultCenter = {
-    lat: location.lat, lng: location.lng
-  };
-
+function MapComponent() {
   return (
-    <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={13}
-        center={defaultCenter}
-      >
-        <Marker position={defaultCenter} />
-      </GoogleMap>
-    </LoadScript>
+    <>
+    <address> 
+     Pune, Maharashtra
+    </address>
+    <div >
+  <iframe src="https://www.google.com/maps/embed?pb=!1m1
+  8!1m12!1m3!1d121059.04711156091!2d73.78056543154418!3d18.52459
+  859950238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2
+  e67461101%3A0x828d43bf9d9ee343!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin
+  !4v1727531678473!5m2!1sen!2sin" width="600" height="450" 
+   allowfullscreen="" loading="lazy" 
+  referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    {/* <address>
+      Chennai, Tamil Nadu
+    </address>
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248756.
+    13131228788!2d80.04419819043572!3d13.04747331623444!2m3!1f0!2f0!3f0!3m2!1i1
+    024!2i768!4f13.1!3m3!1m2!1s0x3a5265ea4f7d3361%3A0x6e61a70b6863d433!2sChenn
+    ai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1727532463878!5m2!1sen!2sin" 
+    width="600" height="450" allowfullscreen="" 
+    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   </div> */}
+    </>
   );
-};
+}
 
 export default MapComponent;
